a5ab630c8a515f865c5514f1d4292fbc74b5de2d
Kemal98 <mokskemal@gmail.com>
2023-12-02 03:04:41 +0100

    update

--- components/Booking/Cars.jsx
+++ components/Booking/Cars.jsx
@@ -3,6 +3,8 @@
 import ListCar from "@/data/ListCar"
 import Image from 'next/image'
 import { useState } from "react"
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
 
 const Cars = () => {
 
@@ -13,7 +15,15 @@ const Cars = () => {
     <div className="mt-2 ">
      <h2 className="font-semibold text-[18px]">Select cars</h2>
       <div className="grid grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-2 items-center justify-center  mt-2">
-           {ListCar.map((car, index)=> (
+      {/* <Carousel
+        showThumbs={false}
+        autoPlay
+        infiniteLoop
+        interval={2000}
+        showArrows={false}
+        showStatus={false} */}
+      {/* > */}
+        {ListCar.map((car, index)=> (
              <div 
               onClick={()=>{setSelectCar(index)}}
               className={`border-[1px] p-2 rounded-md ${selectCart == index ? 'border-yellow-500 border-[2px]' : ''} hover:border-yellow-400 cursor-pointer hover:scale-105 transition-transform duration-200`}  key={car.id} >
@@ -24,6 +34,27 @@ const Cars = () => {
            </div>
            </div>
            ))}
+               {/* {ListCar.map((image) => (
+          // <Image 
+          //   src={image.image}
+          //   // alt={image.alt}
+          //   width={484}
+          //   height={484}
+          //   className="object-contain"
+          //   key={image.alt}
+          // />
+          <div 
+          onClick={()=>{setSelectCar(index)}}
+          className={`border-[1px] p-2 rounded-md ${selectCart == index ? 'border-yellow-500 border-[2px]' : ''} hover:border-yellow-400 cursor-pointer hover:scale-105 transition-transform duration-200`}  key={car.id} >
+          <Image className="w-full" src={car.image} width={200} height={150}/>
+          <div className="py-2  flex justify-between">
+          <h2 className="text-[15px] text-gray-500 font-semibold">{car.name}</h2>
+          <span className="text-black font-medium">{car.charges * 8} $</span>
+       </div>
+       </div>
+        ))}
+      </Carousel> */}
+          
       </div>
     </div>
   )
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,8 +8,8 @@ const NavBar = () => {
         <div className='flex items-center gap-10'>
             <Image src={'/logo.png'} alt='logo' width={170} height={100}/>
             <ul className='hidden md:flex gap-6'>
-                <li className='hover:bg-gray-100 p-2 rounded-md cursor-pointer transition-all'>Home</li>
-                <li className='hover:bg-gray-100 p-2 rounded-md cursor-pointer transition-all'>History</li>
+                {/* <li className='hover:bg-gray-100 p-2 rounded-md cursor-pointer transition-all'>Home</li>
+                <li className='hover:bg-gray-100 p-2 rounded-md cursor-pointer transition-all'>History</li> */}
             </ul>
         </div>
         <UserButton afterSignOutUrl='/'/>
